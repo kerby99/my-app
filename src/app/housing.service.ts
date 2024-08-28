ff2cddbda8aaa1aaf0db40d9cbe76a67fac08835
Kerby <kerby99.tineus@gmail.com>
2024-08-28 14:44:41 -0400

    Adding a form to the app

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -11,8 +11,15 @@ export class HousingService {
   getAllHousingLocations(): HousingLocation[] {
     return this.housingLocationList;
   }
+  
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
+  }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`,
+    );
   }
 
   readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
